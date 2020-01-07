d72684b1442236fa275f21a160ff2beb650ec397
wangeen <wangeen@gmail.com>
2020-01-07 17:14:21 +0800

    Update Longest-Substring-Without-Repeating-Characters.cpp

--- C++/Longest-Substring-Without-Repeating-Characters.cpp
+++ C++/Longest-Substring-Without-Repeating-Characters.cpp
@@ -1,19 +1,20 @@
+// l. 用map存储之前出现过的char的位置
+// 2. 用pre记录不重复周期的起始点，注意要与自己永远取max
+
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        if (s.empty()) return 0;
-        int left = 0, len = 0;
-        vector<int> vis(256, 0);
-        for (int i = 0; i < s.size(); i ++) {
-            if (vis[s[i]] > 0) {
-                while (vis[s[i]] > 0) {
-                    vis[s[left]] --;
-                    left ++;
-                }
+        if(s.size()<2) return s.size();
+        int max_len = 0;
+        map<char, int> pos;
+        int pre = -1;
+        for(int i=0; i<s.size(); ++i){
+            if(pos.find(s[i])!=pos.end()){
+                pre = max(pos[s[i]], pre); 
             }
-            len = max(i - left + 1, len);
-            vis[s[i]] ++;
+            max_len = max(max_len, i-pre);
+            pos[s[i]] = i;
         }
-        return len;
+        return max_len;
     }
 };
