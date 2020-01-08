89edee2a6a8bdd6dc6283ca823340e3095041c6c
wangeen <wangeen@gmail.com>
2020-01-08 11:18:04 +0800

    Update Median-of-Two-Sorted-Arrays.cpp

--- C++/Median-of-Two-Sorted-Arrays.cpp
+++ C++/Median-of-Two-Sorted-Arrays.cpp
@@ -1,17 +1,36 @@
+// 关键是理解算法的思路，在left，right的约束下不停的更新分割位置。
+
 class Solution {
 public:
-	double FindKth(vector<int> &nums1, vector<int> &nums2, int startA, int startB, int k) {
-		if (startA >= nums1.size()) return nums2[startB + k - 1];
-		if (startB >= nums2.size()) return nums1[startA + k - 1];
-		if (k == 1) return min(nums1[startA], nums2[startB]);
-		int valA = startA + k / 2 - 1 < nums1.size() ? nums1[startA + k / 2 - 1] : INT_MAX;
-		int valB = startB + k / 2 - 1 < nums2.size() ? nums2[startB + k / 2 - 1] : INT_MAX;
-		if (valA < valB) return FindKth(nums1, nums2, startA + k / 2, startB, k - k / 2);
-		else return FindKth(nums1, nums2, startA, startB + k / 2, k - k / 2);
-	}
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int len = nums1.size() + nums2.size();
-        if (len & 1) return FindKth(nums1, nums2, 0, 0, len / 2 + 1);
-        else return (FindKth(nums1, nums2, 0, 0, len / 2) + FindKth(nums1, nums2, 0, 0, len / 2 + 1)) / 2.0;
+        int x = nums1.size();
+        int y = nums2.size();
+        if(x>y) return findMedianSortedArrays(nums2, nums1);
+        x = nums1.size();
+        y = nums2.size();
+       //  cout << " " << x <<" " << y << endl;
+        int s = 0;
+        int e = x;
+        while(s<=e){
+            int px = (s+e)/2;
+            int py = (x+y+1)/2-px;
+            int lX = (px==0)?INT_MIN:nums1[px-1];
+            int hX = (px==x)?INT_MAX:nums1[px];
+            int lY = (py==0)?INT_MIN:nums2[py-1];
+            int hY = (py==y)?INT_MAX:nums2[py];
+            // cout << lX <<" " << hX <<" " << lY <<" " << hY << endl;
+            if(lX<=hY && lY<=hX){
+                if((x+y)%2==0){
+                    return (max(lX, lY) + min(hX, hY)) / 2.0;
+                }else{
+                    return max(lX, lY);
+                }
+            }else if(hX<lY){
+                s = px+1;
+            }else{
+                e = px-1;
+            }
+        }
+        return 0;
     }
 };
